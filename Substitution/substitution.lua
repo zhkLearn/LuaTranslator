2b09340c690cd073735e0839d5f817c69f184379
ZHK <zhk.tiger@gmail.com>
2018-11-20 20:01:55 +0800

    strTim

--- Substitution/substitution.lua
+++ Substitution/substitution.lua
@@ -27,6 +27,23 @@ function Literalize(str)
     return str:gsub("[%(%)%.%%%+%-%*%?%[%]%^%$]", function(c) return "%" .. c end)
 end
 
+function case_insensitive_pattern(pattern)
+
+  -- find an optional '%' (group 1) followed by any character (group 2)
+  local p = pattern:gsub("(%%?)(.)", function(percent, letter)
+
+    if percent ~= "" or not letter:match("%a") then
+      -- if the '%' matched, or `letter` is not a letter, return "as is"
+      return percent .. letter
+    else
+      -- else, return a case-insensitive character class of the matched letter
+      return string.format("[%s%s]", letter:lower(), letter:upper())
+    end
+
+  end)
+
+  return p
+end
 
 -- Concat the contents of the parameter list,
 -- separated by the string delimiter (just like in perl)
@@ -67,6 +84,18 @@ function strSplit(delimiter, text)
 	return list
 end
 
+function strTrim(s)
+   return s:match('^%s*(.*%S)') or ''
+end
+
+function strTrimLeft(s)
+   return s:match('^%s*(.*)') or ''
+end
+
+function strTrimRight(s)
+   return s:match('(.*%S)') or ''
+end
+
 -- Print the Usage to the console
 function Usage()
   print("使用方法: lua.exe substitution.lua in.txt dictionary.txt out.txt")
@@ -91,7 +120,7 @@ function main()
         Usage()
         return
     end
-
+	
     local in_source     = arg[1]
     local in_dictionary = arg[2]
     local out_dest      = arg[3]
@@ -111,7 +140,7 @@ function main()
     end
 
     printf("处理中...\n")
-
+	
     -- 读入原始文件
     local in_scource_handle = io.open(in_source, "rb")
     local in_source_data   = in_scource_handle:read("*a")
@@ -119,15 +148,21 @@ function main()
     io.close(in_scource_handle)
 	
 	
+    -- 读入字典文件
 	local in_dict_handle = io.open(in_dictionary, "rb");
     local in_dict_data   = in_dict_handle:read("*a")
     io.close(in_dict_handle)
 	
+    -- 替换
 	for _, line in pairs(strSplit('\n', in_dict_data)) do
 		local vects = strSplit('|', line)
 		if #vects == 2 then
+			local keyTrim = strTrim(vects[1])
+			local valTrim = strTrim(vects[2])
 			--print(vects[1], vects[2])
-			in_source_data = in_source_data:gsub(Literalize(vects[1]), vects[2])
+			if keyTrim:len() > 0 and valTrim:len() > 0 then
+				in_source_data = in_source_data:gsub(case_insensitive_pattern(Literalize(vects[1])), vects[2])
+			end
 		end
 	end
 
